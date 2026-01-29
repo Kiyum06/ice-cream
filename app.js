b3b328380d2e8cba19b9dbadc86fe480576c8702
Kiyum <kiyum@Kiyums-MacBook-Air.local>
2026-01-29 14:52:31 -0800

    done with node app

---
+++ app.js
@@ -0,0 +1,20 @@
+// Import the express module
+import express from 'express';
+
+// Create an instance of an express application
+const app = express();
+
+//Define a port number where our server will listen
+const PORT = 3000; 
+
+app.use(express.static('public'));
+
+// Define our main route ('/')
+app.get('/', (req, res) => {
+    res.sendFile(`${import.meta.dirname}/views/home.html`);
+});
+
+// Start server and listed on the specified port
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+});
---
+++ public/js/ice-cream.js
@@ -0,0 +1,48 @@
+// alert("Hello, ice cream!");
+
+
+document.getElementById("icecream-form").onsubmit = () => {
+
+    clearErrors();
+
+    let isValid = true;
+    //validate name
+    let name = document.getElementById("name").value.trim();
+    if(!name) {
+        document.getElementById("err-name").style.display = "block";
+        isValid = false;
+    }
+
+    //validate email address
+    let email = document.getElementById("email").value.trim();
+    if(!email && !"@") {
+        document.getElementById("err-email").style.display = "block";
+        isValid = false;
+    }
+
+     //validate flavor
+    let size = document.getElementById("flavor").value;
+    if(size == "none") {
+        document.getElementById("err-flavor").style.display = "block";
+        isValid = false;
+    }
+
+    //validate cone
+    let waffle = document.getElementById("waffle");
+    let sugar = document.getElementById("sugar");
+    let cup = document.getElementById("cup");
+    if(!waffle.checked && !sugar.checked && !cup.checked) {
+        document.getElementById("err-cone").style.display = "block";
+        isValid = false;
+    }
+
+    return isValid;
+
+}
+
+function clearErrors() {
+    let errors = document.getElementsByClassName("err");
+    for(let i = 0; i < errors.length ; i++){
+        errors[i].style.display = "none";
+    }
+}
