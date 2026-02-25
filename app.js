706e024248a9d489fa6809d44f1dff10b0c8de25
Kiyum <kiyum1406@gmail.com>
2026-02-25 13:40:18 -0800

    done

--- app.js
+++ app.js
@@ -9,10 +9,43 @@ const PORT = 3006;
 
 app.use(express.static('public'));
 
+// Set EJS as the view engine
+app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({extended: true}));
+
+const orders = [];
+
 // Define our main route ('/')
 app.get('/', (req, res) => {
-    res.sendFile(`${import.meta.dirname}/views/home.html`);
+    res.render('home');
 });
+
+// admin route ('/')
+app.get('/admin', (req, res) => {
+    res.render('admin', { orders });
+});
+
+// Define /sumbit-order route ('/')
+app.post('/submit-order', (req, res) => {
+
+    // create a json object to store the order data
+    const order = {
+        name: req.body.name,
+        email: req.body.email,
+        cone: req.body.cone,
+        flavor: req.body.flavor,
+        toppings: req.body.toppings ? req.body.toppings: "none",        
+        comment: req.body.comment,
+        timestamp: new Date()
+    };
+
+    // Add order object to orders array 
+    orders.push(order);
+    res.render('confirmation', { order });
+});
+
+
 
 // Start server and listed on the specified port
 app.listen(PORT, () => {
